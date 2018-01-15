f3cfc2cd06e4e2cef9396af1af79fcc0fa808481
Alvaro Marquina <thesunno@gmail.com>
2018-01-14 22:10:25 -0400

    Created cannon rotation and shoot effect

---
+++ Assets/Scripts/cannon.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cannon : MonoBehaviour {
+
+	public float speed = 200;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float rotation = -1 * Input.GetAxis("Horizontal") * Time.deltaTime * speed;
+		transform.Rotate(0, 0, rotation);
+	}
+
+}
---
+++ Assets/Scripts/shoots.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shoots : MonoBehaviour {
+
+	private Renderer [] renderers;
+	public float timeBetweenShoots;
+	// Use this for initialization
+	void Start () {
+		renderers = gameObject.GetComponentsInChildren<Renderer>();
+		foreach(Renderer renderer in renderers){
+			renderer.enabled = false;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown("space") || Input.GetButtonDown("Fire1")) {
+			InvokeRepeating("fire", 0, timeBetweenShoots);
+		}
+		if (Input.GetKeyUp("space") || Input.GetButtonUp("Fire1")) {
+			foreach(Renderer renderer in renderers){
+				CancelInvoke("fire");
+				renderer.enabled = false;
+			}
+		}
+	}
+
+	void fire(){
+		foreach(Renderer renderer in renderers){
+			renderer.enabled = !renderer.enabled;
+		}
+	}
+}
